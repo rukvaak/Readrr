11bc65e50774186ec9eae71a64c1cef9413bb87c
pavan1994rg <pavan1994rg@gmail.com>
2020-05-15 21:13:20 +0530

    comitted with reducer pattern

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component}  from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
@@ -7,17 +7,36 @@ import SigninScreen from './components/signincomponent';
 import FormExample from './components/signincomponent';
 import SignupScreen from './components/signupcomponent';
 import LanguageScreen from './components/langcomponent';
+import CategoriesPage from './components/categories';
+import {createStore,applyMiddleware, compose}  from 'redux';
+import   reducer    from './Services/Reducer/reducer'
+import dataService from './Services/data-service'
+import thunk from 'redux-thunk'
+const store = createStore(reducer, applyMiddleware(thunk));
+import {Provider} from 'react-redux';
 const navigator = createStackNavigator(
   {
    Login:LoginScreen,
    Signin:SigninScreen,
    Signup:SignupScreen,
-   Language:LanguageScreen
+   Language:LanguageScreen,
+   Categories:CategoriesPage
   },
   {
-    initialRouteName: 'Language',
+    initialRouteName: 'Login',
   }
 );
 
 const AppContainer = createAppContainer(navigator);
-export default AppContainer;
+
+export default class App extends Component {
+  render () {
+    return (
+        <Provider store={store}>
+          <AppContainer/>
+         
+        </Provider>
+    )
+  }
+}
+
